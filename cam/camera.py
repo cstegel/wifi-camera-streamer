b862d460061f3b548caccd54f0e075172aca5580
Cory <cstegel@gmail.com>
2015-06-09 22:22:27 -0400

    cleanup

--- cam/camera.py
+++ cam/camera.py
@@ -8,34 +8,15 @@ import sys
 from cam import network
 from functools import partial
 
-# 
-# while(True):
-#   # Capture frame-by-frame
-#   ret, frame = cap.read()
-# 
-#   # Our operations on the frame come here
-#   gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-# 
-#   # Display the resulting frame
-#   cv2.imshow('frame',frame)
-#   keycode = cv2.waitKey(1)
-#   print(keycode)
-#   if keycode & 0xFF == ord('q'):
-#     break
-# 
-# # When everything done, release the capture
-# cap.release()
-# cv2.destroyAllWindows()
-
 
 HOST_IP = '127.0.0.1'
 HOST_PORT = 8479
 
 def get_frame(video_capture, ip):
   ret, frame = video_capture.read()
-  print('\n'.join(dir(frame)))
-  print(frame.dtype)
-  print(frame.shape)
+  #print('\n'.join(dir(frame)))
+  #print(frame.dtype)
+  #print(frame.shape)
   
   return frame.tostring()
 
@@ -47,8 +28,8 @@ if __name__ == '__main__':
   ip = sys.argv[3] or HOST_IP
   
   if sys.argv[1] == 'server':
-    cap = cv2.VideoCapture(-1)
-    # cap.open(0)
+    cap = cv2.VideoCapture(0)
+    
     if not cap.isOpened():
       print('\n'.join(dir(cap)))
       print('Could not connect to video camera')
@@ -62,7 +43,7 @@ if __name__ == '__main__':
       client = network.Client(socket.SOCK_STREAM)
       data = client.receive(ip, port)
       frame = np.fromstring(data, 'uint8')
-      print(frame.shape)
+      #print(frame.shape)
       frame = frame.reshape(480, 640, 3)
       cv2.imshow('frame', frame)
       if cv2.waitKey(1) & 0xFF == ord('q'):
