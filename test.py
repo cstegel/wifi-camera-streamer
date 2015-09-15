135a4be7684ec56bd39ecb7d88b2fda441cb6e59
Cory Stegelmeier <cstegel@gmail.com>
2015-09-15 16:55:09 -0400

    updated test script

--- test.py
+++ test.py
@@ -1,7 +1,14 @@
+import time
 import cv2
 
 cv2.namedWindow("preview")
 vc = cv2.VideoCapture(0)
+
+vc.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+vc.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+
+print('res: %sx%s, FPS: %s' % (vc.get(cv2.CAP_PROP_FRAME_WIDTH),
+  vc.get(cv2.CAP_PROP_FRAME_HEIGHT), vc.get(cv2.CAP_PROP_FPS)))
 
 if vc.isOpened(): # try to get the first frame
   print('opened')
@@ -10,10 +17,21 @@ else:
   print('not opened')
   rval = False
 
+frame_count = 0
+start_time = time.time()
+
 while rval:
   cv2.imshow("preview", frame)
-  rval, frame = vc.read()
-  key = cv2.waitKey(20)
+  rval, frame = vc.read(frame)
+  key = cv2.waitKey(1)
   if key == 27: # exit on ESC
     break
+
+  frame_count += 1
+  if frame_count >= 30:
+    end_time = time.time()
+    print('%ffps' % (frame_count / (end_time - start_time)))
+    frame_count = 0
+    start_time = end_time
+
 cv2.destroyWindow("preview")
