de62561afe068bdbf67c7aecf5f21ee7b669ec8e
Cory Stegelmeier <cstegel@gmail.com>
2015-09-21 23:51:06 -0400

    start_cam_stream.sh takes an arg for the video device to capture from

--- mjpg-streamer/start_cam_stream.sh
+++ mjpg-streamer/start_cam_stream.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 # must be run from directory with mjpg_streamer binary
 # eg:  ~/git/mjpg-streamer/mjpg-streamer-experimental
-if [ $# -ne 1 ]; then
+if [ $# -ne 2 ]; then
   me=`basename "$0"`
-  echo "Usage: $me <path to mjpg_streamer binary folder (that contains the plugins folder as well)>"
+  echo "Usage: $me <directory containing mjpg_streamer binary> </dev/video device # capture from>"
+  echo "   ex: $me ~/git/mjpg-streamer-mjpg-streamer 0"
+  echo "     - this uses /dev/video0"
   exit 1
 fi
 
 export LD_LIBRARY_PATH=$1
-$1/mjpg_streamer -i "input_uvc.so --device /dev/video0 -r 1280x720 --fps 60" -o "output_http.so -p 8090 -w ./www"
+$1/mjpg_streamer -i "input_uvc.so --device /dev/video$2 -r 1280x720 --fps 60" -o "output_http.so -p 8090 -w ./www"
